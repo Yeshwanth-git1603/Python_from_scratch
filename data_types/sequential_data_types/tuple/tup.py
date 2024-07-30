375350b6d952e069175234a987f334c3d2f0bb66
Yeshwanth Kasula <yeshwanthkasula1603@gmail.com>
2024-07-30 19:26:13 +0530

    python tup.py

--- data_types/sequential_data_types/tuple/tup.py
+++ data_types/sequential_data_types/tuple/tup.py
@@ -11,3 +11,11 @@ type_cast=list(t1)
 type_cast.append("blue whale")
 t=tuple(type_cast)
 print(t)
+
+#tuple slicing
+print(t[0:1])
+print(t[0:2])
+print(t[0:3])
+print(t[0:4])
+print(t[0:5])
+print(t[2:5])
