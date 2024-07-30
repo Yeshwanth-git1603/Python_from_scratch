02d9d8decd3ecc215c7ca3faf3933522487f03ed
Yeshwanth Kasula <yeshwanthkasula1603@gmail.com>
2024-07-30 19:21:17 +0530

    python.py

--- data_types/sequential_data_types/tuple/tup.py
+++ data_types/sequential_data_types/tuple/tup.py
@@ -4,3 +4,10 @@
 t=("whale","shark","tortoise","humpback")
 print(t)
 
+#tuple type casting
+
+t1=("whale","shark","stingray","killerwhale")
+type_cast=list(t1)
+type_cast.append("blue whale")
+t=tuple(type_cast)
+print(t)
