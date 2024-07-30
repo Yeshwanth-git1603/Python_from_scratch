fe368094d767b3f70d79c7c2d9fb58d79d9ce198
EC2 Default User <ec2-user@ip-172-31-18-79.ec2.internal>
2024-07-30 10:06:28 +0000

    python

---
+++ operators/assignment_op/assignment_op.py
@@ -0,0 +1,35 @@
+#assignment operators
+# +=,-=,*=,/=,==
+
+x=10
+y=20
+x+=x
+
+print(x)
+print(y)
+x*=x
+print(x)
+y*=y
+print(y)
+x/=x
+print(x)
+y/=y
+print(y)
+x==y
+print(x)
+x%=x
+print(x)
+y%=y
+print(y)
+#bitwise assignment operators
+x=5
+y=10
+
+x&=x
+print(x)
+y&=y
+print(y)
+x|=y
+print(x)
+y|=x
+print(y)
