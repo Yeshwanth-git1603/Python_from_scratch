4bfeca47e3b6f92bd6128e8e871d14b9241c0335
Yeshwanth Kasula <yeshwanthkasula1603@gmail.com>
2024-07-31 23:03:49 +0530

    employee_sal_check.py

--- control_flow_statements/conditional_statemnts/if_else/if_else_examples/employee_sal_check.py
+++ control_flow_statements/conditional_statemnts/if_else/if_else_examples/employee_sal_check.py
@@ -1,4 +1,4 @@
-#employee salary check
+# checking the employee salary using if else
 
 Basic_salary=int(input("enter the basic pay:"))
 HRA=(Basic_salary*(20/100))
@@ -8,17 +8,33 @@ Gross_salary=Basic_salary+HRA+DA
 Net_salary=Gross_salary-I_tax
 
 if Basic_salary <= 30000:
+    
+    HRA=(Basic_salary*(20/100))
+    DA=(Basic_salary*(10/100))
+    I_tax=(Basic_salary*(5/100))
+    
     print("the employee gross salary is",Gross_salary)
     print("the employee basic pay after deductions is:",Net_salary)
 
 elif Basic_salary > 30000 and Basic_salary <= 50000:
+    
+    HRA=(Basic_salary*(25/100))
+    DA=(Basic_salary*(15/100))
+    I_tax=(Basic_salary*(10/100))
+    
     print("the employee gross salary is",Gross_salary)
     print("the employee basic pay after deductions is:",Net_salary)
     
 elif Basic_salary > 50000 and Basic_salary <= 100000:
+    HRA=(Basic_salary*(30/100))
+    DA=(Basic_salary*(20/100))
+    I_tax=(Basic_salary*(15/100))
     print("the employee gross salary is",Gross_salary)
     print("the employee basic pay after deductions is:",Net_salary)
 elif Basic_salary>100000:
+    HRA=(Basic_salary*(35/100))
+    DA=(Basic_salary*(25/100))
+    I_tax=(Basic_salary*(20/100))
     print("the employee gross salary is",Gross_salary)
     print("the employee basic pay after deductions is:",Net_salary)
 else:
