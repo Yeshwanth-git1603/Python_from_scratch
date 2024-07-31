e734e06e6953b691f6d922bff3ce497e3aba661e
Yeshwanth Kasula <yeshwanthkasula1603@gmail.com>
2024-07-31 11:44:08 +0530

    neg_or_pos.py

--- control_flow_statements/conditional_statemnts/if_else/if_else_examples/neg_or_pos.py
+++ control_flow_statements/conditional_statemnts/if_else/if_else_examples/neg_or_pos.py
@@ -1,11 +1,8 @@
 # checking the number whether it is positive or negative
 
 a=int(input("enter a value:"))
-b=int(input("enter b value:"))
-if a<0:
-    print("negative is a:",a)
-elif b<0:
-    print("negative is b:",b)
+if a>=0
+    print("a is positive:",a)
 else:
-    print("enter the valid number")
+    print("a is negative")
 
