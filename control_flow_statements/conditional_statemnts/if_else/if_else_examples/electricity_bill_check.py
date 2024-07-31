e22d03ec720def607a99beba1d3e54ef532b5ffd
Yeshwanth Kasula <yeshwanthkasula1603@gmail.com>
2024-07-31 22:50:16 +0530

    electricity_bill_check.py

--- control_flow_statements/conditional_statemnts/if_else/if_else_examples/electricity_bill_check.py
+++ control_flow_statements/conditional_statemnts/if_else/if_else_examples/electricity_bill_check.py
@@ -3,25 +3,21 @@
 meter_no=int(input("enter the meter number:"))
 previous_reading=int(input("enter the previous reading:"))
 current_reading=int(input("enter the current_reading:"))
-units=int(input("enter the units:"))
+total_units=current_reading-previous_reading
+total_amount=total_units*unit_charge
+
 
 if units <=100:
     unit_charge=2.50
-    total_units=current_reading-previous_reading
     print("the total units consumed is:",total_units)
-    total_amount=total_units*unit_charge
     print("the total amount is:",total_amount)
 elif units > 100 and units <= 150:
     unit_charge=3.50
-    total_units=current_reading-previous_reading
     print("the total units consumed is:",total_units)
-    total_amount=total_units*unit_charge
     print("the total amount is:",total_amount)
 elif units > 150 and units <=200:
     unit_charge=4.50
-    total_units=current_reading-previous_reading
-    print("the total units consumed is:",total_units)
-    total_amount=total_units*unit_charge
+    print("the total units consumed is:",total_units)    
     print("the total amount is:",total_amount)
         
         
@@ -34,9 +30,7 @@ elif units >200 and units <=300:
         
 elif units >300:
     unit_charge=6.50
-    total_units=current_reading-previous_reading
     print("the total units consumed is:",total_units)
-    total_amount=total_units*unit_charge
     print("the total amount is:",total_amount)
 
 else:
