287ca4f169654a53b6c933585149a61795082761
Yeshwanth Kasula <yeshwanthkasula1603@gmail.com>
2024-08-03 17:23:52 +0530

    reduce with function.py

---
+++ functions/lambda_functions/reduce_function.py/reduce2.py
@@ -0,0 +1,13 @@
+#using reduce function
+from functools import reduce
+def red(n):
+    s=0
+    for x in range(n+1):
+        s=s+x
+        res=red(10)
+print("the sum of the seq is:",res)
+
+#lambda reduce function
+
+x=reduce(lambda x,y:x+y,l1)
+print("the sum of the list is:",x)
