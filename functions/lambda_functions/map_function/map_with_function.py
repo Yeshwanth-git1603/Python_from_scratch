5c0a083775c1c419f299a840ddeeaf068062a075
Yeshwanth Kasula <yeshwanthkasula1603@gmail.com>
2024-08-03 17:25:01 +0530

    map_with_function.py

---
+++ functions/lambda_functions/map_function/map_with_function.py
@@ -0,0 +1,13 @@
+#using map fuction 
+def map_fun(n):
+    return n*n
+
+for x in range(10+1):
+    res=map_fun(x)
+    print(res,end=" ")
+    
+l1=[10,20,30,40,50]
+#lambda map function
+
+x=list(map(lambda x:x*x,l1))
+print(x)
