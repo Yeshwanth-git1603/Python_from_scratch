2024cefaab2fef79d96b56525cfd61a73a0b253b
Yeshwanth Kasula <yeshwanthkasula1603@gmail.com>
2024-08-04 19:11:36 +0530

    f_operations.py

--- file_hadling/file_operations/f_operations.py
+++ file_hadling/file_operations/f_operations.py
@@ -1,12 +1,16 @@
 #file handling
+#WriteMode
+
 f=open("super_cars.txt","w")
 f.write("1.koenigesgg CC850,2.Pagani Zonda,3.Koenigessg Jesko,4.Ferrari SF90,5.Buggati chiron")
 f.writelines=["6.lamborghini aventador svj"]
 f.close()
+#ReadMode
 
 f=open("super_cars.txt","r")
 read=f.read()
 print(read)
+#UsingWithKeyword
 
 with open("super_cars.txt","a") as f:
     f.write("6.lamborghini aventador svj 7.Aston martin db12 ,8.porsche 911 Gt3rs")
@@ -23,7 +27,7 @@ print(f.read())
 
 with open("koenigsegg.txt","w") as file:
     file.write(" koenigsegg cars: CC850 , jesko , chimera , regera , agera ,agera rs , one")
-    file.close
+    file.close()
     
 with open("koenigsegg.txt","r") as file:
     print(file.read())
