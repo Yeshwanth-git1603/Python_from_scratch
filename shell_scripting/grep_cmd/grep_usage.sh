45d9d6aed6cc93914ae4d7a8e5f21fcc00b5f412
EC2 Default User <ec2-user@ip-172-31-18-79.ec2.internal>
2024-08-09 09:30:25 +0000

    shell_scripting

---
+++ shell_scripting/grep_cmd/grep_usage.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+hello there how are you
+hi good morning
+hello have a good day
+bad times create great men
+
+
+vi grep_usage.sh | grep -v "you"
+
+vi grep_usage.sh | grep -w "you"
+
+
+vi grep_usage.sh | grep -n "you"
